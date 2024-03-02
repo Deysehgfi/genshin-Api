d948286c8eb824da71eed33e11e17b94485f39bb
Deyse <138785041+Deysehgfi@users.noreply.github.com>
2024-03-02 16:37:33 -0300

    Add files via upload

--- js/person.js
+++ js/person.js
@@ -15,166 +15,39 @@ document.addEventListener('DOMContentLoaded' , ()=>{
          return Response.json()
      })
     .then((data)=>{
+        console.log(data)
 
       personagem(data)})
     // .catch((err)=>console.error(err))})
 
     
     function personagem(item){
-        const personagemImg = document.querySelector('.personagem-img')
+        const personagemImg = document.querySelector('.img-personagem')
         const nome = document.querySelector('.nome')
+        const tipoModelo = document.querySelector('.tipo_modelo')
         const raridade = document.querySelector('.raridade')
         const arma= document.querySelector('.arma')
         const titulo = document.querySelector('.titulo')
+        const visao= document.querySelector('.visao')
+        const aniversario =document.querySelector('.aniversario')
+        const regiao =document.querySelector('.regiao')
        
     
     personagemImg.src = `./img/foto${paramIndex}.jpg` 
-   nome.innerHTML =`Nome :${item.result.name}`
-    raridade.innerHTML =`raridade :${item.result.rarity}`
-    arma.innerHTML =`arma :${item.result.weapon}`
-    titulo.innerHTML =`titulo: ${item.result.title}`
 
+   nome.innerHTML =`${item.result.name}`
+    tipoModelo.innerHTML =`TIPO DE MODELO: ${item.result.model_type}`
+    raridade.innerHTML =`RARIDADE: ${item.result.rarity}`
+    arma.innerHTML =`ARMA: ${item.result.weapon}`
+    titulo.innerHTML =`TITÚLO: ${item.result.title}`
+    visao.innerHTML =`VISÃO: ${item.result.vision}`
+    aniversario.innerHTML =`ANIVERSÁRIO: ${item.result.birthday}`
+    regiao.innerHTML =`REGIÃO: ${item.result.region}`
+    
        
     
     }})
 
-
-
-//     function personagem(item){
-//         const personagemImg = document.querySelector('.personagem-img')
-//         const nome = document.querySelector('.nome')
-//         const raridade = document.querySelector('.raridade')
-//         const arma = document.querySelector('.arma')
-
-//         personagemImg.src = `./img/foto${paramIndex}.jpg`
-//         nome.innerHTML =`Nome :${item.name}`
-//         raridade.innerHTML =`raidade:${item.rarity}`
-//         peso.innerHTML =`arma:${item.weapon}`}
-//     // function listarCaracteristias(characters){
-//   document.getElementById("persona").innerHTML = `${}`
-   
-
-       
-
-//         const divPersonagemDados = document.createElement('div')
-//         divPersonagemDados.innerHTML= `
-//   <div class = "personagem-dados">
-//        <img class="img-personagem" src= "./img/foto${index}.jpg">
-//       <div> 
-//          <h3 class="name-person">${item.name}</h3>
-//       </div> 
-//   </div>`;
-//     })
-//      .catch((err)=>console.log(err))
-
-    
-    //  function personagem(item){
-        // <span class="raridade">rarity</span>
-        // <span class="arma">weapon</span>
-        // <span class="visão">vision</span>
-
-
-
-        // <span class="resultado">result</span>
-        // <span class="raridade">rarity</span>
-        // <span class="arma">weapon</span>
-        // <span class="visao">vision</span>
-        // <span class="modelo">modeltype</span>
-        // <span class="aniversario">birthday</span>
-        // <span class="constelacao">constellation</span>
-
-//         const personagemImg = document.querySelector('.personagem-img')
-//         const nome = document.querySelector('.nome')
-//         const raridade = document.querySelector('.raridade')
-//         const arma = document.querySelector('.arma')
-//         const visao = document.querySelector('.visao')
-//         const modelo = document.querySelector('.modelo')
-//         const aniversario = document.querySelector('.aniversario')
-//         const constelacao = document.querySelector('.constelacao')
-
-//     personagemImg.src = `./img/foto${paramIndex}.jpg`
-//         nome.innerHTML =`Nome :${item.name}`
-//         raridade.innerHTML =`Raridade :${item.rarity}`
-//         arma.innerHTML =`Arma :${item.weapon}[]`
-//         visao.innerHTML =`Visão:${item.vision}`
-//         modelo.innerHTML =`Modelo:${item.model_type}`
-//         aniversario.innerHTML =`Aniversário:${item.birthday}`
-//         constelacao.innerHTML =`constelação:${item.constellation}`
-       
-
-//    }
-//  })
-
- 
-//  divPersonagens.innerHTML= `
-//  <div class = "personagem-dados">
-//       <img class="img-personagem" src= "./img/foto${index}.jpg">
-//      <div> 
-//          <h3 class="name-person">${item.name}</h3>
-//       </div> 
-//  </div>`;
-
-// const page = 1
-// const baseURL = "https://gsi.fly.dev/";
-// const loadCharacter = async ()=> {
-//     const res = await fetch(`${baseUrl}character?page=${page}`);
-//     const data = await res.json();
-//     const limitData = data.results.slice(0,6)
-//      return  { results:limitData}
-     
-// };
-// const loadperson = async () => {
-//     const res = await fetch(`${baseUrl}/character?${id.character}`)
-//     return await res.json()};
-
-
-// const loadAllWithPromiseAll = async () => {
-//     const[character,person] = await Promise.all([
-// loadCharacter(),
-// loadperson(),])
-
-// console.log("character", character)
-// console.log("person", person)}
-
-// document.addEventListener('DOMContentLoaded',()=>{
-//     const urlParam = new URLSearchParams(window.location.search)
-//     // console.log(urlParam.get('index'))
-//     const paramIndex= urlParam.get('index')
-//     //console.log(paramIndex) valor do index do parâmetro
-//     const url = `https://gsi.fly.dev/character${parseInt(paramIndex)+1}`
-    
-//     fetch(url)
-//     .then((Response)=>{
-//         if(!Response.ok){
-//             throw new Error('Error de rewde: código'+ Response.status)
-//     }
-//         return Response.json()
-//     })
-//     .then((data)=>{
-//        personagem(data)//
-//     })
-//     .catch((err)=>console.log(err))
-//     function personagem(item){
-//         const personagemImg = document.querySelector('.personagem-img')
-//         const nome = document.querySelector('.nome')
-//         const raridade = document.querySelector('.raridade')
-//         const arma = document.querySelector('.arma')
-//         const visao = document.querySelector('.visao')
-//         const modelo = document.querySelector('.modelo')
-//         const aniversario = document.querySelector('.aniversario')
-//         const constelacao = document.querySelector('.constelacao')
-
-//     personagemImg.src = `./img/foto${paramIndex}.jpg`
-//         nome.innerHTML =`Nome :${item.name}`
-//         raridade.innerHTML =`Raridade :${item.rarity}`
-//         arma.innerHTML =`Arma :${item.weapon}kg`
-//         visao.innerHTML =`genero:${item.vision}`
-//         modelo.innerHTML =`cor do cabelo :${item.model_type}`
-//         aniversario.innerHTML =`cor dos olhos :${item.birthday}`
-//         constelacao.innerHTML =`cor da pele :${item.constellation}`
-       
-//     }
-// })
 
 
 
