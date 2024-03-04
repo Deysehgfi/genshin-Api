e40301a64c53994901ab3227d276b5dbcdb6e12a
Deyse <138785041+Deysehgfi@users.noreply.github.com>
2024-03-04 12:21:49 -0300

    Add files via upload

--- js/index.js
+++ js/index.js
@@ -20,58 +20,6 @@ document.addEventListener('DOMContentLoaded' , ()=>{
      })     .catch((err)=>console.log(err))
  })
 
-
- const btn = document.querySelector('#btn')
-
-btn.addEventListener('click',()=>{
-  window.location = './page/home.html'
-//     const baseURL ="https://gsi.fly.dev/characters?page=2"
-
-//    fetch(baseURL)
-//    .then((Response)=>{
-//        if(!Response.ok){
-// throw new Error('erro de rede! código:'+Response.status)
-
-// } return Response.json()    })
-
-
-//  .then((data)=>{
-//   console.log(data.results[0].name)
-// renderizarPersonagens2(data)
-//     })     .catch((err)=>console.log(err))
-
-    
-})
-function renderizarPersonagens2(items){
-  const container = document.getElementById('personagem-container')
-items.results.forEach((item, index)=>{
- const divPersonagens = document.createElement('div')
- divPersonagens.innerHTML= `
- <div class = "personagem-caixa">
-      <img class="img-personagem" src= "./img/foto${index}.png">
-     <div> 
-         <h3 class="name-person">${item.name}</h3>
-         
-
-      </div> 
- </div>`;
-divPersonagens.addEventListener('click',()=>{
-detalhesPersonagem(index)
-})
- divPersonagens.classList.add('personagem')
- container.appendChild(divPersonagens)
-
-})
-console.log(items) 
-}
-
-function detalhesPersonagem(index){
-window.location.href= `./person.html?index=${index}`}
-
-    
-
-
-
  
  function ListarPersonagens(items){
      const container = document.getElementById('personagem-container')
@@ -86,7 +34,7 @@ window.location.href= `./person.html?index=${index}`}
 
          </div> 
     </div>`;
-  divPersonagens.addEventListener('click',()=>{
+  divPersonagens.addEventListener('click',()=>{ 
    detalhesPersonagem(index)
    })
     divPersonagens.classList.add('personagem')
